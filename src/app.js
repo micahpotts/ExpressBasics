a64bd6a6b91e38de6f474dbce17b3e05a8de2e2b
Micah Potts <Micah Potts>
2017-02-15 22:55:47 -0500

    Added route, serves up content.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+      posts = require('./mock/posts.json');
 
 var app = express();
 
-app.get('/', function(request, response) {
-  response.send("Listening!! Port 3000!!");
+app.get('/', function(req, res) {
+  res.send("<h1>I am listening!! Port 3000!!</h1>");
 });
 
-app.listen(3000);
+app.get('/blog', function(req, res) {
+  res.send(posts);
+});
+
+app.listen(3000, function() {
+  console.log("The FRONTEND server is listening on port 3000!!")
+});
