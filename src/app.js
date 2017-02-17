72d524bdfe5e7c105cc3171419357c7a071c9c28
Micah Potts <mrs.micahpotts@gmail.com>
2017-02-17 10:33:00 -0500

    Request and receive blog objects via url

--- src/app.js
+++ src/app.js
@@ -9,8 +9,10 @@ app.get('/', function(req, res) {
   res.send("<h1>I am listening!! Port 3000!!</h1>");
 });
 
-app.get('/blog', function(req, res) {
-  res.send(posts);
+app.get('/blog/:title', function(req, res) {
+  var title = req.params.title;
+  var post = posts[title];
+  res.send(post);
 });
 
 app.listen(3000, function() {
